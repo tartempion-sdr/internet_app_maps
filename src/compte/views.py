e56a6a13e417f79e4e24555bb4541d904f6d9602
tartempion-sdr <93546844+tartempion-sdr@users.noreply.github.com>
2023-07-14 14:46:26 +0200

    suite acces

--- src/compte/views.py
+++ src/compte/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .forms import CreerUtilisateur
+from django.contrib import messages
 
 # Create your views here.
 def inscriptionPage(request):
@@ -9,12 +11,23 @@ def inscriptionPage(request):
         form=CreerUtilisateur(request.POST)
         if form.is_valid():
             form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request,'compte creer avec succes '+user)
             return redirect('acces')
     context={'form':form}
     return render(request,'compte/inscription.html',context)
 
 def accesPage(request):
     context={}
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('maps')
+        else:
+            messages.info(request,"erreur nom d'utilisateur")
     return render(request,'compte/acces.html',context)
 
 
